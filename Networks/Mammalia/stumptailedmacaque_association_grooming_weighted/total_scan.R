08f863bfeff8033a19f8cbbabc41241188d7c71d
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-04-17 10:51:28 +0900

    Added and finished from Griffin&Nunn stumptailed macaques

---
+++ Networks/Mammalia/stumptailedmacaque_association_grooming_weighted/total_scan.R
@@ -0,0 +1,5 @@
+total_scan<- 3800; # 10 scans of 3 min over total time = 190h => 190*20
+export_to_graphml(old.path = "C:/R/Git/asnr/Networks/Mammalia/primates_association_weighted/weighted_griffin_primate_matrix_8_macacaarctoides.graphml",
+                  new.path = "C:/R/Git/asnr/Networks/Mammalia/stumptailedmacaque_association_grooming_weighted/weighted_griffin_primate_matrix_8_macacaarctoides_modified.graphml",
+                  correcting.factor = 25/6.849132)
+# max(Adj = 6.849132), max(raw)= 25
